6b9e0d0eee25e2379ad0fe81ee637d641f6ca14a
u1and0 <e01.ando60@gmail.com>
2023-05-10 07:32:46 +0900

    [feat] add another query form

--- static/main.js
+++ static/main.js
@@ -1,15 +1,33 @@
 "use strict";
-function search() {
-    const responseElem = document.getElementById("response");
-    if (responseElem === null)
-        return;
-    responseElem.innerHTML = "";
-    const regulationElem = document.getElementById("regulation");
-    if (regulationElem === null)
-        return;
+const responseElem = document.getElementById("response");
+const regulationElem = document.getElementById("regulation");
+const details = document.querySelector("details");
+const anotherForm = document.getElementById("another-form");
+function createAnotherForm() {
+    const form = document.createElement("form");
+    form.setAttribute("onsubmit", "moreSearch(); return false;");
+    form.setAttribute("id", "another-form");
+    const input = document.createElement("input");
+    input.setAttribute("type", "search");
+    input.setAttribute("id", "next-keyword");
+    input.setAttribute("placeholder", "詳細についてさらに質問します");
+    input.setAttribute("size", "64");
+    const submit = document.createElement("input");
+    submit.setAttribute("type", "submit");
+    submit.setAttribute("id", "next-submit");
+    submit.setAttribute("value", "&#xf002;");
+    submit.setAttribute("class", "fas");
+    form.appendChild(input);
+    form.appendChild(submit);
+    document.body.appendChild(form);
+}
+function refresh() {
     regulationElem.innerHTML = "";
-    const details = document.querySelector("details");
+    responseElem.innerHTML = "";
     details.removeAttribute("open");
+}
+function search() {
+    refresh();
     const keyword = document.getElementById("keyword").value;
     if (keyword === null)
         return;
@@ -33,5 +51,6 @@ function search() {
             regulationElem.textContent = data.regulation;
         }, 20);
     })
+        .then(createAnotherForm)
         .catch((error) => console.error(`Error:${error}`));
 }
--- static/src/main.ts
+++ static/src/main.ts
@@ -1,14 +1,39 @@
+const responseElem = document.getElementById("response");
+const regulationElem = document.getElementById("regulation");
+const details = document.querySelector("details");
+const anotherForm = document.getElementById("another-form");
+
+// 関連規約についてさらに質問するフォームを作成
+function createAnotherForm() {
+  const form = document.createElement("form");
+  form.setAttribute("onsubmit", "moreSearch(); return false;");
+  form.setAttribute("id", "another-form");
+  const input = document.createElement("input");
+  input.setAttribute("type", "search");
+  input.setAttribute("id", "next-keyword");
+  input.setAttribute("placeholder", "詳細についてさらに質問します");
+  input.setAttribute("size", "64");
+  const submit = document.createElement("input");
+  submit.setAttribute("type", "submit");
+  submit.setAttribute("id", "next-submit");
+  submit.setAttribute("value", "&#xf002;");
+  submit.setAttribute("class", "fas");
+  form.appendChild(input);
+  form.appendChild(submit);
+  document.body.appendChild(form);
+}
+
+// Reset page
+function refresh() {
+  regulationElem.innerHTML = "";
+  responseElem.innerHTML = "";
+  details.removeAttribute("open");
+  // anotherForm.innerHTML = "";
+}
+
 // バックエンドに規約と回答を問い合わせ
 function search() {
-  // Reset page
-  const responseElem = document.getElementById("response");
-  if (responseElem === null) return;
-  responseElem.innerHTML = "";
-  const regulationElem = document.getElementById("regulation");
-  if (regulationElem === null) return;
-  regulationElem.innerHTML = "";
-  const details = document.querySelector("details");
-  details.removeAttribute("open");
+  refresh();
   // Search keyword
   const keyword = document.getElementById("keyword").value;
   if (keyword === null) return;
@@ -31,5 +56,6 @@ function search() {
         regulationElem.textContent = data.regulation;
       }, 20);
     })
+    .then(createAnotherForm)
     .catch((error) => console.error(`Error:${error}`));
 }

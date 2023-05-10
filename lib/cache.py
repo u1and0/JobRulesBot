fa8535c56fe7648430b900b1b320c8df7bc07021
u1and0 <e01.ando60@gmail.com>
2023-05-11 07:31:52 +0900

    [test] not passed

--- lib/cache.py
+++ lib/cache.py
@@ -8,7 +8,31 @@ CachedResponse = namedtuple("CachedResponse", ["vectorframe", "response"])
 
 @dataclass
 class Cache:
-    cache_dict: dict[str, CachedResponse] = field(default_factory=dict)
+    """
+    Usage:
+
+    >>> cache = Cache()
+
+    # キャッシュに値を保存する
+    >>> cache.set("key1", "value1")
+
+    # キャッシュから値を取得する
+    >>> cache.get("key1")
+    "value1"
+
+    # キャッシュに値を保存する
+    >>> cache["key2"] = "value2"
+
+    # キャッシュから値を取得する
+    >>> cache["key2"]
+    "value2"
+
+    # 存在しないキーを指定して値を取得する
+    >>> cache.get("not_exist_key", "default_value")
+    "default_value"
+    """
+    # dict[query: ["vectorframe", "response"]]
+    cache_dict: dict[str, Any] = field(default_factory=dict)
 
     @lru_cache(maxsize=2)
     def get(self, key: str, default: Any = None) -> Any:
@@ -34,3 +58,8 @@ class Cache:
 
     def __repr__(self) -> str:
         return str(self)
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()

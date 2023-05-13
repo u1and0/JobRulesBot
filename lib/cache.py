471e2dd846ae098873f5af84920fc0395f906e02
u1and0 <e01.ando60@gmail.com>
2023-05-13 21:36:42 +0900

    [fix] Cache Class using OderedDict in self.data

--- lib/cache.py
+++ lib/cache.py
@@ -1,60 +1,73 @@
-from typing import Any, Optional
-from collections import namedtuple, UserDict
-from functools import lru_cache
+from collections import OrderedDict
+from typing import Optional
+from collections import OrderedDict, namedtuple
 
 CachedResponse = namedtuple("CachedResponse", ["vectorframe", "response"])
 
 
-class Cache(UserDict):
+class Cache:
     """
     Usage:
 
-    >>> cache = Cache()
+    >>> cache = Cache(maxsize=3)
 
     # キャッシュに値を保存する
-    >>> cache.set('key1', 'value1')
+    >>> cache["key1"] = "value1"
 
     # キャッシュから値を取得する
-    >>> v1 = cache.get('key1')
-    >>> v1 == 'value1'
-    True
-
-    # キャッシュに値を保存する
-    >>> cache['key2'] = 'value2'
-
-    # キャッシュから値を取得する
-    >>> cache['key2']
-    'value2'
+    >>> cache['key1']
+    'value1'
 
     # 存在しないキーを指定して値を取得する
     >>> cache.get('not_exist_key', 'default_value')
     'default_value'
+
+    # インスタンス化
+    >>> cache = Cache(maxsize=3, a=1, b=2, c=3)
+
+    >>> cache
+    OrderedDict([('a', 1), ('b', 2), ('c', 3)])
+
+    # maxsizeを超えると
+    >>> cache["d"] = 4
+
+    # 古いものから削除("a"の要素がなくなる)
+    >>> cache
+    OrderedDict([('b', 2), ('c', 3), ('d', 4)])
+
+    # 参照すると
+    >>> cache["c"]
+    3
+
+    # 新しく追加したものとして最後尾に追加
+    >>> cache
+    OrderedDict([('b', 2), ('d', 4), ('c', 3)])
     """
-    def __hash__(self):
-        """ @lru_cacheをget()につけるために必要。
-        __hash__メソッドはオブジェクトをハッシュ可能にするためのメソッドです。
-        このメソッドを実装することで、@lru_cacheが適用できるようになります。
-        ただし、この実装は単純にオブジェクトのIDをハッシュ値として使用しているため、
-        別のオブジェクトによってIDが再利用された場合に誤った結果が得られる可能性があります。"""
-        return id(self)
+    def __init__(self, maxsize: Optional[int] = None, **kwargs):
+        self.maxsize = maxsize
+        self.data = OrderedDict(**kwargs)
 
-    @lru_cache(maxsize=3)
-    def __getitem__(self, key, default=None):
-        """ 最長未使用頻度。最も長く使用されていないものから破棄
-        100個まで値を保持可能。
-        """
-        return self.data.get(key, default)
+    def __getitem__(self, key):
+        value = self.data.pop(key)
+        self.data[key] = value
+        return value
 
-    def set(self, key, value):
+    def __setitem__(self, key, value):
+        if key in self.data:
+            self.data.pop(key)
+        elif len(self.data) >= self.maxsize:
+            self.data.popitem(last=False)
         self.data[key] = value
 
-    def __str__(self) -> str:
-        return "Cache({})".format(str(self.data))
+    def get(self, key, default=None):
+        if key in self.data:
+            return self.__getitem__(key)
+        return default
 
-    def __repr__(self) -> str:
-        return str(self)
+    def __repr__(self):
+        return repr(self.data)
 
 
 if __name__ == "__main__":
     import doctest
-    doctest.testmod()
+    doctest.testmod(verbose=True)

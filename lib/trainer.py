5aa19221be84ad1c25f8ced1622b055f27867cf3
u1and0 <e01.ando60@gmail.com>
2023-05-16 13:31:13 +0900

    [feat] training write/read file

--- lib/trainer.py
+++ lib/trainer.py
@@ -13,7 +13,6 @@ from openai.embeddings_utils import get_embedding
 
 from . import VectorFrame
 
-MAX_TOKENS = 400
 MODEL = "text-embedding-ada-002"
 ENC = tiktoken.encoding_for_model(MODEL)
 
@@ -30,7 +29,7 @@ def load_data(strings: list[str]) -> VectorFrame:
     return VectorFrame(df)
 
 
-def split_tokens(title: str, text: str) -> list[str]:
+def split_tokens(title: str, text: str, max_tokens: int) -> list[str]:
     """規則タイトルと本文を結合したリストを返す"""
     # 改行を削除
     text = re.sub(r'\n|\r', '', text).strip()
@@ -39,25 +38,26 @@ def split_tokens(title: str, text: str) -> list[str]:
     # トークン化
     tokens = ENC.encode(text)
     # 最大トークン数で分割
-    chunks = chunked(tokens, MAX_TOKENS)
+    chunks = chunked(tokens, max_tokens)
     # 規則タイトルと本文を結合
     title_text_tokens = [f"{title} {ENC.decode(c)}" for c in chunks]
     return title_text_tokens
 
 
-def split_text(title_text: dict[str, str]) -> list[str]:
+def split_text(title_text: dict[str, str], max_tokens: int) -> list[str]:
     """規則のタイトルと本文を結合して、最大トーン数で分割する"""
     # 分割された規則テキストを格納するリスト
     training_data_split = []
     # 規則のタイトルと本文を結合して、最大トーン数で分割する
     for k, v in title_text.items():
         # 分割されたテキストをリストに追加
-        training_data_split.extend(split_tokens(k, v))
+        training_data_split.extend(split_tokens(k, v, max_tokens))
     return training_data_split
 
 
-def training(rules: pd.DataFrame, savefile: str,
-             **kwargs) -> Optional[pd.DataFrame]:
+def training(rules: VectorFrame,
+             savefile: Optional[str] = None,
+             **kwargs) -> Optional[VectorFrame]:
     """emmbedding して文字列をベクトル化します。
     実行にはOpenAI APIを使用します。
     トークンサイズに基づいた料金がかかるので
@@ -75,14 +75,16 @@ def training(rules: pd.DataFrame, savefile: str,
     rules["embedding"] = rules.text.apply(
         lambda x: get_embedding(x, engine=MODEL))
 
-    # 処理データを保存
-    rules.to_json(savefile, force_ascii=False, orient="records", **kwargs)
-    return rules
+    if savefile:
+        # 処理データを保存
+        rules.to_json(savefile, force_ascii=False, orient="records", **kwargs)
+    else:
+        return VectorFrame(rules)
 
 
 if __name__ == "__main__":
     # training_data.jsonの読み込み
-    with open("data/inner/train_data_from_dneo.json", "r") as f:
+    with open("data/model_list.json", "r") as f:
         rules = json.load(f)
         # rules = {
         #     "title1": "text1",
@@ -91,10 +93,17 @@ if __name__ == "__main__":
         # }
         # text n は1000トークン以上の長さ
 
-    train_list = split_text(rules)
+    # 400トークンごとに分割
+    train_list = split_text(rules, 400)
+
     # stdoutへ出力
     print(train_list[:3])
 
-    # 分割された規則テキストのリストをファイルに保存
-    # with open("data/inner/training_data_split.json", "w") as f:
-    #     json.dump(training_data_split, f, ensure_ascii=False)
+    # As VectorFrame
+    vf = load_data(train_list)
+
+    # ベクトル化して結果をdata/result.jsonへ保存
+    training(vf, savefile="./data/result.json")
+
+    # Read result
+    vf = VectorFrame(pd.read_json())
